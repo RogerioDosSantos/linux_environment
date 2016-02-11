40d4b380f1d29311be76dd79445f0b958032f60e
roger <rogerio.santos.main@gmail.com>
2016-02-11 08:36:58 -0600

    Updated Links

--- bash/setup_linux_environment_links.sh
+++ bash/setup_linux_environment_links.sh
@@ -3,12 +3,12 @@
 echo Setting up Links:
 
 echo - dot files / folders
-ln -s ~/git/linux_environment ~/.linux
-ln -s ~/.linux/dot_folders/vim ~/.vim
-ln -s ~/.linux/dot_folders/vimrc ~/.vimrc
-ln -s ~/.linux/dot_folders/bash/bashrc ~/.bashrc
-ln -s ~/.linux/dot_folders/bash/aliases ~/.bash_aliases
+ln -s -n ~/git/linux_environment ~/.linux
+ln -s -n ~/.linux/dot_folders/vim ~/.vim
+ln -s -n ~/.linux/dot_folders/vimrc ~/.vimrc
+ln -s -n ~/.linux/dot_folders/bash/bashrc ~/.bashrc
+ln -s -n ~/.linux/dot_folders/bash/aliases ~/.bash_aliases
 
 echo - Shortcuts
-ln -s ~/.linux/bash ~/bash
+ln -s -n ~/.linux/bash ~/bash
 
