a0879457e99283ac1d9ec7c39051562a7a0fbb5a
Rogerio dos Santos <rogerio.santos.main@gmail.com>
2016-04-24 02:21:18 -0400

    Added tmux configuration file

--- bash/setup_linux_environment_links.sh
+++ bash/setup_linux_environment_links.sh
@@ -7,6 +7,7 @@ ln -s -n ~/git/linux_environment ~/.linux
 ln -s -n ~/.linux/dot_folders/vim ~/.vim
 ln -s -n ~/.linux/dot_folders/vimrc ~/.vimrc
 ln -s -n ~/.linux/dot_folders/vimrc.bundles ~/.vimrc.bundles
+ln -s -n ~/.linux/dot_folders/tmux.conf ~/.tmux.conf
 
 echo - Shortcuts
 ln -s -n ~/.linux/bash ~/bash
