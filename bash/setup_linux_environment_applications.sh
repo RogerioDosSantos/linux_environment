1de807314c3078822ac5bc61165eb504376b9c90
Rogerio dos Santos <roger@localhost.localdomain>
2016-05-16 14:34:59 -0400

    Changed the application to install clang from the website to use the last binaries

--- bash/setup_linux_environment_applications.sh
+++ bash/setup_linux_environment_applications.sh
@@ -1,34 +1,71 @@
 #!/bin/bash
+cd "$(dirname "$0")"
 
-echo "Getting Programs (You will need to run as super user)"
+echo "### Linux Environment Application Setup ###"
 
-echo "- apt-get"
+echo ""
+echo "--- Defining variables ---"
+
+user_bin_dir="${HOME}/bin"
+echo "User Directory: ${user_bin_dir}"
+
+echo ""
+echo "--- Getting Programs ---" 
+
+echo: "Note: You will need to run as super user)"
+
+echo ""
+echo "- apt-get -"
 sudo apt-get update
 sudo apt-get upgrade
 
-echo "- Git"
+echo ""
+echo "- Git -"
 sudo apt-get -y install git
 
-echo "- Sync and Backup"
+echo ""
+echo "- Sync and Backup -"
 sudo apt-get -y install rsync
 
-echo "- Chryptography"
+echo ""
+echo "- Chryptography -"
 sudo apt-get -y install encfs
 
-echo "- Build Essentials"
+echo ""
+echo "- Build Essentials -"
 sudo apt-get -y install build-essential cmake python-dev
 
-echo "- TMUX"
+echo ""
+echo "- TMUX -"
 sudo apt-get -y install tmux
 
-echo "- NodeJS"
+echo ""
+echo "- NodeJS -"
 sudo apt-get -y install nodejs npm
 sudo npm -y install -g typescript
 
-echo "- Text Editors"
+echo ""
+echo "- Text Editors -"
 sudo apt-get -y install vim
 
-echo " - Programming tools"
+echo ""
+echo " - Programming tools -"
 sudo apt-get -y install silversearcher-ag
 sudo apt-get -y install ctags
-sudo apt-get -y install libclang1-3.5
+# sudo apt-get -y install libclang1-3.5
+
+echo ""
+echo " - Clang"
+
+mkdir -p "${user_bin_dir}"
+cd ${user_bin_dir}
+mkdir -p "./temp"
+wget -O "./temp/clang_intaller.tar.xz" "http://llvm.org/releases/3.8.0/clang+llvm-3.8.0-x86_64-linux-gnu-ubuntu-14.04.tar.xz"
+tar xvfJ "./temp/clang_intaller.tar.xz" -C .
+mv "./clang+llvm-3.8.0-x86_64-linux-gnu-ubuntu-14.04" "./clang_3.8.0"
+rm -rf "./temp"
+ln -s "./clang_3.8.0/bin/clang" .
+ln -s "./clang_3.8.0/bin/clang-format" .
+
+
+
