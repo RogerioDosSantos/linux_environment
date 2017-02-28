71754ecc6cd1db064aad6484447bb6be7129531a
Roger <roger.santos.main@gmail.com>
2017-02-27 19:19:14 -0600

    Updated Linux environment to be hosted by the linux vm

--- bash/create_links_docker_machine.sh
+++ bash/create_links_docker_machine.sh
@@ -2,10 +2,14 @@
 
 echo Setting up Links:
 
+echo - Linux Environment -
+rm -f ~/.linux
+ln -s -n ~/git/linux_environment ~/.linux
+
 echo - dot files / folders
 rm -f ~/.tmux.conf
-ln -s -n ~/host/git/scripts/tmux/tmux.conf ~/.tmux.conf
+ln -s -n ~/.linux/tmux/tmux.conf ~/.tmux.conf
 
 echo - Shortcuts
 rm -f ~/bash
-ln -s -n ~/host/git/scripts/bash ~/bash
+ln -s -n ~/.linux/bash ~/bash
