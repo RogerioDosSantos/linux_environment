da20ef6e1911ae50c6c29425bbd4eb79ca21cc83
Roger <roger.santos.main@gmail.com>
2018-01-02 16:10:15 -0600

    Moved all scripts from linux to scripts project

--- bash/mount_virtualbox_host_folder.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo Creating and Mounting Host
-mkdir -p ~/host
-sudo mount -t vboxsf host ~/host
-
--- bash/mount_virtualbox_safe_folder.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo Creating and Mounting Safe
-mkdir -p ~/.safe ~/safe
-sudo mount -t vboxsf Safe ~/.safe
-encfs ~/.safe ~/safe
